46996273ab873e5677adde9f9e8272c88bc2129b
Demetrius Michael <arrrwalktheplank@gmail.com>
2025-08-20 13:42:48 -0400

    test: Add softmax tests for all dimensions

--- test/activations.jl
+++ test/activations.jl
@@ -5,6 +5,11 @@ import NNlib: softmax as nn_softmax
 @testset "Activations" begin
     x = randn(5, 4)
     @testset "softmax ≈ NNlib.softmax" begin
-        @test softmax(x, dims=2) ≈ nn_softmax(x, dims=2)
+        @testset "column-wise (dims=1)" begin
+            @test softmax(x, dims=1) ≈ nn_softmax(x, dims=1)
+        end
+        @testset "row-wise (dims=2)" begin
+            @test softmax(x, dims=2) ≈ nn_softmax(x, dims=2)
+        end
     end
 end
