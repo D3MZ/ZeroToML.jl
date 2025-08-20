6fbcda2945083714534c353f92d26651a97c3c6b
Demetrius Michael <arrrwalktheplank@gmail.com>
2025-08-20 13:24:42 -0400

    chore: Reorganize Adam export statement

--- src/ZeroToML.jl
+++ src/ZeroToML.jl
@@ -9,12 +9,10 @@ include("encoding.jl")
 export build_vocab, encode, decode, positional_encoding
 
 include("optimizers.jl")
-export Adam
-
 include("transformers.jl")
 export LayerNorm, ScaledDotProductAttention, MultiHeadAttention, FeedForward, TransformerBlock, Transformer,
        cross_entropy_loss, cross_entropy_loss_backward,
-       backward, backward!, zero_gradients!, update!,
+       backward, backward!, zero_gradients!, update!, Adam,
        generate
 
 end
