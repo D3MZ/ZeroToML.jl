308cc4d17ae2177c05f441feba9686ec8b7e2cdb
D3MZ <arrrwalktheplank@gmail.com>
2025-10-06 17:05:07 -0400

    feat: Inject log SNR into first layer and expand channels

--- src/diffusion.jl
+++ src/diffusion.jl
@@ -11,7 +11,7 @@ glorot_conv(w, h, c_in, c_out) = (rand(Float32, w, h, c_in, c_out) .* 2f0 .- 1f0
 "Initialize fully convolutional network parameters for image-to-image noise prediction"
 function conv_parameters(d)
     kernel_size = 3
-    channels = [1, 1]
+    channels = [1, 16, 32, 16, 1]
     c_out = channels[2]
     W_alpha_bar = reshape(glorot(c_out, 1), 1, 1, c_out, 1)
     layers = []
@@ -30,8 +30,8 @@ function predict(m, x, t, ᾱ)
     h = reshape(x, H, W, 1, 1)
     padding = (size(first(m.layers).W, 1) - 1) ÷ 2
 
-    # First layer with ᾱ injection
-    h = conv(h, m.layers[1].W; pad=padding) .+ m.layers[1].b .+ m.W_alpha_bar .* ᾱ[t]
+    # First layer with log snr ᾱ injection
+    h = conv(h, m.layers[1].W; pad=padding) .+ m.layers[1].b .+ m.W_alpha_bar .* snr(ᾱ)[t]
     
     if length(m.layers) > 1
         h = relu(h)
@@ -56,6 +56,8 @@ noise_schedule(T; βmin=1f-4, βmax=0.02f0) = range(βmin, βmax; length=T)
 signal_schedule(β::AbstractRange) = 1 .- β
 "the total remaining signal variance is the cumprod of the signal_schedule"
 remaining_signal(α::AbstractRange) = cumprod(α)
+"Log Signal to Noise Ratio"
+snr(ᾱ) = log.(ᾱ ./ (1 .- ᾱ))
 "Conditional marginal mean E[xₜ | x₀] for the forward diffusion process q(xₜ | x₀)"
 marginal_mean(x, ᾱ, t) = sqrt(ᾱ[t]) .* x
 "Conditional marginal noise for the forward diffusion marginal q(xₜ | x₀). This is the random Gaussian noise part added to the deterministic mean √ᾱₜ · x₀."
@@ -132,16 +134,22 @@ scale(imgs::AbstractVector) = map(scale, imgs)
 
 # Below is just a scratch pad -- will delete after
 using Test, Plots, BenchmarkTools
+
 Random.seed!(42)
 H,W = 16, 16
 d = H*W
-dataset = scale(boxes(H, W))
+dataset = shuffle(scale(boxes(H, W)))
 
 T = 1_000
 β = noise_schedule(T)
 α = signal_schedule(β)
 ᾱ = remaining_signal(α)
 model = conv_parameters(d)
+
+# Why log.(ᾱ ./ (1 .- ᾱ) and not ᾱ 
+# plot(ᾱ, label = "ᾱ[t]") # Signal is compressed near 0 after 500 steps
+# plot(ᾱ ./ (1 .- ᾱ), label = "SNR(t) = ᾱ[t] / (1 - ᾱ[t])") # Large dynamic range, but signal explodes on each end
+# plot(log.(ᾱ ./ (1 .- ᾱ)), label = "log SNR(t) = log(ᾱ[t] / (1 - ᾱ[t]))") # Signal is compressed when SNR is low, and gives a large dynamic range afterwards
 
 # Calculate loss before training on a sample
 x0_test = rand(dataset)
@@ -150,10 +158,10 @@ t_test = rand(1:T)
 xt_test = noised_sample(x0_test, ᾱ, t_test, ε_test)
 untrained_loss = loss(model, xt_test, t_test, ε_test, ᾱ)
 
-η = 1f-1
 epochs = 10
-diffusion_train(model, ᾱ, T, η, dataset, epochs)
-
+model = diffusion_train(model, ᾱ, T, 1f-1, shuffle(dataset), epochs)
+# model = diffusion_train(model, ᾱ, T, 1f-2, shuffle(dataset), epochs)
+# model = diffusion_train(model, ᾱ, T, 1f-3, shuffle(dataset), epochs)
 
 # @code_warntype diffusion_train(model, ᾱ, T, η, dataset, epochs)
 # using BenchmarkTools
@@ -164,7 +172,7 @@ diffusion_train(model, ᾱ, T, η, dataset, epochs)
 # @test trained_loss < untrained_loss
 
 # # Reshape to 2-D and plot
-heatmap(first(dataset),
+heatmap(rand(dataset),
         color=:grays,
         aspect_ratio=:equal,
         title="Random generated box")
@@ -182,3 +190,35 @@ plots = [heatmap(samples[i],
 plot(plots...;
      layout=(5,2),
      size=(300,500))
+     
+# H = W = isqrt(d)
+# x = randn(Float32, H, W)
+# μ = similar(x)
+# m = model
+# μs = []
+
+# for t in T:-1:1
+#     ε̂ = predict(m, x, t, ᾱ)
+#     μ = posterior_mean(x, ε̂, β, α, ᾱ, t)
+#     push!(μs,μ)
+#     x = latent(μ, β, t, x)
+# end
+
+# N = length(μs)
+# frames = 120
+# idxs = round.(Int, N .- (N-1) .* exp.(-5.0 .* range(0,1,length=frames)))
+# idxs = unique(idxs)
+# idxs[end] = N
+# anim = @animate for i in idxs
+#     t = T - i + 1
+#     heatmap(μs[i];
+#             title = "t = $t",
+#             color=:grays,
+#             axis=false,
+#             framestyle=:none,
+#             xticks=false,
+#             yticks=false,
+#             aspect_ratio = :equal)
+# end
+
+# mp4(anim, "denoising.mp4", fps = 30)
