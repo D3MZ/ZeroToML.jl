0ed89411ee63afa34a89dde924469070c85f09cf
D3MZ <arrrwalktheplank@gmail.com>
2025-10-09 13:19:44 -0400

    feat: Add Tullio convolution with padding

--- src/convolution.jl
+++ src/convolution.jl
@@ -1,6 +1,7 @@
-using Tullio, LoopVectorization, Flux, Test
+using Tullio, LoopVectorization, Flux, Test, BenchmarkTools
 
-"Convolution with padding via Tullio"
+"Tullio Convolution"
 convolution(x,k) = @tullio y[i+_, j+_] := x[i+a, j+b] * k[a,b]
+"Tullio Convolution with Padding"
 convolution(x,k;p=padding) = @tullio y[i+_, j+_] := x[pad(i-a,p), pad(j-b,p)] * k[a,b]
 
