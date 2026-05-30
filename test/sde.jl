0b8156a6cd30b52efb912930400e7813026f8aaa
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-05-30 17:40:52 -0400

    Start SDE denoising from terminal time

--- test/sde.jl
+++ test/sde.jl
@@ -24,7 +24,7 @@ using Plots
     H, W = 12, 12
     h, w = 3, 3
     η = 1f-2
-    t = 0.75f0
+    t = 1f0
     rng = MersenneTwister(1)
     dataset = shuffle(rng, boxes(H, W, h, w))
     sde = VPSDE(βmin=0.1f0, βmax=2f0)
