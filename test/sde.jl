42ba375d2ffef9002dc3b58a4c86cba61322f6e9
Demetrius Michael <arrrwalktheplank@gmail.com>
2026-05-30 17:58:09 -0400

    Test SDE denoising with correlation

--- test/sde.jl
+++ test/sde.jl
@@ -10,6 +10,8 @@ using Plots
     @info "This is testing SDE and denoising score matching: https://arxiv.org/abs/2011.13456"
 
     boxes(H=12, W=12, h=3, w=3) = [(g = -ones(Float32, H, W); g[i:i+h-1, j:j+w-1] .= 1f0; g) for i in 1:H-h+1 for j in 1:W-w+1]
+    center(x) = x .- mean(x)
+    correlate(x, y) = sum(center(x) .* center(y)) / (sqrt(sum(abs2, center(x)) * sum(abs2, center(y))) + eps())
     panel(title, sample) = heatmap(sample; title, color=:grays, clims=(-1, 1), axis=false, colorbar=false, aspect_ratio=:equal)
     Random.seed!(1)
 
@@ -32,8 +34,11 @@ using Plots
     denoised = clamp.(probability_flow_sample(model, sde, input, t; steps=100), -1f0, 1f0)
     input_loss = mean((input .- x₀).^2)
     denoised_loss = mean((denoised .- x₀).^2)
+    input_correlation = correlate(x₀, input)
+    denoised_correlation = correlate(x₀, denoised)
     @info "$label score loss" untrained=untrained_loss trained=trained_loss
     @info "Denoising loss" input=input_loss denoised=denoised_loss
+    @info "Cross correlation" input=input_correlation denoised=denoised_correlation
 
     figure = plot(
         panel("training $label", x₀),
@@ -46,5 +51,5 @@ using Plots
     @info "Saved SDE samples" path=path
 
     @test trained_loss < untrained_loss
-    @test denoised_loss < input_loss
+    @test denoised_correlation > input_correlation
 end
